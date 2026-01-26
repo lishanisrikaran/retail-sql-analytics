ea532163c4427f2c5141e8a72956720aafc81c62
lishanisrikaran <l.srikaran@outlook.com>
2026-01-26 19:41:33 +0000

    Updating Order Value Distribution SQL Query + Analysis

--- 5) Order Value Distribution/SQL Query.sql
+++ 5) Order Value Distribution/SQL Query.sql
@@ -2,19 +2,50 @@ WITH order_values AS (
 -- Collates the total revenue per delivered order
     SELECT
         o.order_id,
-        SUM(oi.price + oi.freight_value) AS order_value
+        SUM(oi.price) + MAX(oi.freight_value) AS order_value
+    FROM orders o
+    JOIN order_items oi
+        ON o.order_id = oi.order_id
+    WHERE o.order_status = 'delivered'
+    GROUP BY o.order_id
+),
+
+ranked_orders AS (
+-- Ranks each order by ascending order value
+    SELECT
+        order_value,
+        ROW_NUMBER() OVER (ORDER BY order_value) AS rn,
+        COUNT(*) OVER () AS total_rows
+    FROM order_values
+)
+
+SELECT
+-- Calculates the mean and median average order values
+    (SELECT ROUND(AVG(order_value), 2) FROM ranked_orders) AS avg_order_value,
+    ROUND(AVG(order_value), 2) AS median_order_value
+FROM ranked_orders
+WHERE rn IN (
+    FLOOR((total_rows + 1) / 2),
+    CEIL((total_rows + 1) / 2)
+);
+
+WITH order_values AS (
+-- Collates the total revenue per delivered order
+    SELECT
+        o.order_id,
+        SUM(oi.price) + MAX(oi.freight_value) AS order_value
     FROM orders o
     JOIN order_items oi
         ON o.order_id = oi.order_id
     WHERE o.order_status = 'delivered'
     GROUP BY o.order_id
 )
-
 SELECT
--- Total orders per R$50 revenue bin interval 
+-- Total orders per every R$50 revenue bin 
     FLOOR(order_value / 50) * 50 AS bin_start,
     FLOOR(order_value / 50) * 50 + 50 AS bin_end,
     COUNT(*) AS order_count
 FROM order_values
 GROUP BY bin_start, bin_end
 ORDER BY bin_start;
+
